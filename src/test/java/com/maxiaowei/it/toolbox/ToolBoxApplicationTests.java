103479441ba7b96cfd7c260cc614b42706939cbb
XiaomA <14351441+wxma@user.noreply.gitee.com>
2025-07-08 21:39:49 +0800

    :tada: 初始化项目

---
+++ src/test/java/com/maxiaowei/it/toolbox/ToolBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxiaowei.it.toolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
